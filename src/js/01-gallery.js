0f9ca819a3e4e19c1434a97b0a92dcc47446e37a
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-09-14 14:25:51 +0300

    hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,8 +3,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
 const galleryRef = document.querySelector(".gallery");
 galleryRef.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 // console.log(galleryRef);
@@ -32,3 +32,21 @@ const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: "alt",
     captionsPosition: "bottom",
     captionDelay: 250, });
+
+
+
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js.
+// Розбий його на декілька підзавдань:
+
+// Додай бібліотеку SimpleLightbox як залежність проекту,
+// використовуючи npm (посилання на CDN з твоєї минулої роботи більше не потрібне).
+// Використовуй свій JavaScript код з попередньої домашньої роботи,
+// але виконай рефакторинг з урахуванням того, що бібліотека була
+// встановлена через npm (синтаксис import/export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно
+// додати ще один імпорт, крім того, що описаний в документації.
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,77 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+
+const formData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
+form.addEventListener("input", throttle(onInput, 500 ));
+form.addEventListener("submit", onSubmit );
+
+
+function onInput (evt) {
+
+    const formData = {
+     email : form.elements.email.value,
+     message : form.elements.message.value
+    };
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+
+}
+
+
+function onSubmit (evt) {
+
+  evt.preventDefault();
+
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  if (email === "" || message === "") {
+    return alert("Please fill in all the fields!");
+  }
+  else {
+    formData.email = email;
+    formData.message = message;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+
+}
+
+
+
+
+
+// HTML містить розмітку форми. Напиши скрипт, який буде зберігати
+// значення полів у локальне сховище, коли користувач щось друкує.
+
+// <form class="feedback-form" autocomplete="off">
+//   <label>
+//     Email
+//     <input type="email" name="email" autofocus />
+//   </label>
+//   <label>
+//     Message
+//     <textarea name="message" rows="8"></textarea>
+//   </label>
+//   <button type="submit">Submit</button>
+// </form>
+
+// Виконуй це завдання у файлах 03-feedback.html і 03-feedback.js.
+// Розбий його на декілька підзавдань:
+
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище
+// об'єкт з полями email і message, у яких зберігай поточні значення полів форми.
+// Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані,
+// заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль
+// об'єкт з полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
+// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
